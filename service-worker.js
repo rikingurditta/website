929db3a578d7f71764d7b03c0d17883682cb9265
Rikin <dolllyamar@gmail.com>
2020-08-27 00:57:24 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e87935bc08fa0a5036759cafbc07ed91.js"
+  "/precache-manifest.c0a51b29b3b5f80a8a8b34405c392d1a.js"
 );
 
 self.addEventListener('message', (event) => {
