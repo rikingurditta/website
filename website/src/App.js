6c3a09cbc97ff44bc37b076aa77a371d123271a8
Rikin <dolllyamar@gmail.com>
2020-08-28 00:43:38 -0400

    Started working on Projects page

--- website/src/App.js
+++ website/src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import './App.scss';
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Homepage from "./pages/Homepage";
+import Projects from "./pages/Projects";
 
 function App() {
     return (
         <div className="App">
-            <Navbar fixed="top" expand="lg">
-                <Navbar.Brand href="#home">Rikin</Navbar.Brand>
-                <Nav className="ml-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
-                </Nav>
-            </Navbar>
             <Router>
+                <Navbar fixed="top" expand="lg">
+                    <Navbar.Brand as={Link} to="/" push>Rikin</Navbar.Brand>
+                    <Nav className="ml-auto">
+                        <Nav.Link as={Link} to="/" push>Home</Nav.Link>
+                        <Nav.Link as={Link} to="/projects" push>Projects</Nav.Link>
+                    </Nav>
+                </Navbar>
                 <Switch>
-                    <Route path="/" component={Homepage}/>
+                    <Route path="/" exact component={Homepage}/>
+                    <Route path="/projects" component={Projects}/>
                 </Switch>
             </Router>
         </div>
---
+++ website/src/pages/Projects.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Container from "react-bootstrap/Container";
+import FullPageRow from "../components/FullPageRow";
+import Col from "react-bootstrap/Col";
+
+let soundcloud_div_style = {
+    fontSize: '10px',
+    color: '#cccccc',
+    lineBreak: 'anywhere',
+    wordBreak: 'normal',
+    overflow: 'hidden',
+    whiteSpace: 'nowrap',
+    textOverflow: 'ellipsis',
+    fontFamily: 'Interstate, Lucida Grande, Lucida Sans Unicode, Lucida Sans, Garuda, Verdana, Tahoma, sans -serif',
+    fontWeight: '100',
+};
+
+
+export default function Projects(props) {
+    return (
+        <Container fluid>
+            <FullPageRow>
+                <Col md={4} className="my-auto">
+                    <h1>Projects</h1>
+                    <h3>I do a lot of stuff</h3>
+                </Col>
+                <Col md={4} className="my-auto">
+                    <img alt="pic of stuff i do"/>
+                </Col>
+            </FullPageRow>
+            <FullPageRow style={{backgroundColor: '#EEEEFF'}}>
+                <Col md={4} className="my-auto">
+                    <iframe width="400px" height="400px" scrolling="no" frameBorder="no" allow="autoplay"
+                            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/487898616&color=%236c4149&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"/>
+                    <div style={soundcloud_div_style}>
+                        <a href="https://soundcloud.com/chocolate-milk-music" title="Chocolate Milk" target="_blank"
+                           style={{color: '#cccccc', textDecoration: 'none',}}>Chocolate Milk</a> · <a
+                        href="https://soundcloud.com/chocolate-milk-music/presidents" title="Presidents" target="_blank"
+                        style={{color: '#cccccc', textDecoration: 'none'}}>Presidents</a></div>
+                </Col>
+                <Col md={4} className="my-auto">
+                    <h1>Music</h1>
+                    <p>I've been producing electronic music and hip hop for a long ass time. Here's something I dropped a couple years ago,
+                    it samples Nas's verses from <i>The World Is Yours</i> as well as clips of Brian Kernighan and DJ Premier.<br/><br/>
+                    <a href="https://soundcloud.com/chocolate-milk-music">Here's my SoundCloud</a></p>
+                </Col>
+            </FullPageRow>
+        </Container>
+    );
+}
