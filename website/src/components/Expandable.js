439c77cb0025ed924aa215ac44ad636812cbd362
Rikin <dolllyamar@gmail.com>
2020-09-02 23:50:18 -0400

    Removed bg colour from Expandable

--- website/src/components/Expandable.js
+++ website/src/components/Expandable.js
@@ -3,7 +3,7 @@ import React, {useState} from 'react';
 export default function Expandable(props) {
     let [expanded, setExpanded] = useState(false);
     return (
-        <div style={{...props.style, backgroundColor: expanded ? '#0000000F' : '#00000000'}}>
+        <div style={props.style}>
             <span onClick={() => setExpanded(!expanded)}
                   style={{cursor: 'pointer', textDecoration: 'underline'}}>
                 {props.inside} {expanded ? '-' : '+'}
