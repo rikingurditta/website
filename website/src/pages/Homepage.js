09a601e01fca9d64ff6385a0f4ceef8262c47c77
Rikin <dolllyamar@gmail.com>
2020-10-06 01:08:47 -0400

    I think I'm done! Don't have any good pics of myself so this is good for now.

--- website/src/pages/Homepage.js
+++ website/src/pages/Homepage.js
@@ -3,7 +3,6 @@ import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import FullPageRow from "../components/FullPageRow";
 import origami_elephant from "../images/origami elephant cropped.jpg";
-import bio_pic from "../images/eric_andre_keyboardist.png";
 import suzanne from "../images/suzanne on fire.png";
 
 export default function Homepage() {
@@ -12,7 +11,10 @@ export default function Homepage() {
             <FullPageRow>
                 <Col md={5} lg={4} className="my-auto">
                     <h1>Hi, I'm Rikin</h1>
-                    <h3>Welcome to my website! It's still under construction.</h3>
+                    <p>
+                        I'm a computer science and mathematics undergrad at University of Toronto. I'm interested in
+                        computer graphics, music production, topology, video games, Rubik's cubes, and cycling.
+                    </p>
                 </Col>
                 <Col md={5} lg={4} className="my-auto" style={{textAlign: 'center'}}>
                     <img src={origami_elephant}
@@ -22,27 +24,15 @@ export default function Homepage() {
             </FullPageRow>
             <FullPageRow style={{backgroundColor: '#FFEEEE'}}>
                 <Col md={5} lg={4} className="my-auto" style={{textAlign: 'center'}}>
-                    <img src={bio_pic} alt="pic of me" style={{width: '80%'}}/>
+                    <img src={suzanne} style={{width: '80%'}} alt="image goes here"/>
                 </Col>
-                <Col md={5} lg={4} className="my-auto">
-                    <h2>About me</h2>
-                    <p>
-                        I'm a computer science and mathematics undergrad at University of Toronto. I'm interested in
-                        computer graphics, music production, topology, video games, and cycling.
-                    </p>
-                </Col>
-            </FullPageRow>
-            <FullPageRow>
                 <Col md={5} lg={4} className="my-auto">
                     <h2>What I'm up to</h2>
                     <p>
                         I'm currently looking for an internship! Aside from that, I'm organizing UTCG events, modelling
                         doughnuts in Blender, making beats, learning abstract algebra, and implementing geometry
-                        processing algorithms. Oh, and working on this site!
+                        processing algorithms.
                     </p>
-                </Col>
-                <Col md={5} lg={4} className="my-auto" style={{textAlign: 'center'}}>
-                    <img src={suzanne} style={{width: '80%'}} alt="image goes here"/>
                 </Col>
             </FullPageRow>
         </Container>
