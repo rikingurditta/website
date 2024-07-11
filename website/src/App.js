0fe097439588534f98007500fa0e019315692f91
Rikin Gurditta <dolllyamar@gmail.com>
2024-07-10 21:11:03 -0400

    Forgot to pass in extpages oops lol

--- website/src/App.js
+++ website/src/App.js
@@ -37,7 +37,7 @@ function App() {
     return (
         <div className="App">
             <Router>
-                <GlobalNavbar sitePages={sitePages}/>
+                <GlobalNavbar sitePages={sitePages} extPages={extPages}/>
                 <ScrollToTop/>
                 <Switch>
                     <Route path="/" exact component={Homepage}/>
